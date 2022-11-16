1943789a360710b1d9697eb1b40992ff7cccc883
GaneshJoshi117 <115364450+GaneshJoshi117@users.noreply.github.com>
2022-11-16 14:01:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,21 +8,20 @@ var score = 20
 document.querySelector('.check').addEventListener('click',
     function () {
         let num = Number.parseInt(document.querySelector('.guess').value);
-
+        
+        if (!num) {document.querySelector('.message').textContent = 'Please enter a number'};
         if (num === number) {
             document.querySelector('.message').textContent = "Correct guess !!!";
             document.querySelector('.number').textContent = `${num}`;
             document.querySelector('body').style.backgroundColor = '#60b347';
             document.querySelector('number').style.width = '30rem';
-//             score--;
             document.querySelector('.score').textContent = score;
             if (score > highScore) {
                 highScore = score;
+                document.querySelector('.highscore').textContent = highScore;
             }
-            document.querySelector('.highscore').textContent = highScore;
-            return null;
         }
-        else {
+        else if(num != number) {
             document.querySelector('.message').textContent = num > number ? "Too high!!" : "Too low!!";
             if (score > 1) {
                 score--;
